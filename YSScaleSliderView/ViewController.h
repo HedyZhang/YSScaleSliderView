4323bab097edfbf99f1a36ea24d93319e085ef94
zhanghaidi <haidi.zhang@quantgroup.cn>
2017-06-04 17:14:47 +0800

    Initial Commit

---
+++ YSScaleSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSScaleSliderView
+//
+//  Created by zhanghaidi on 2017/6/4.
+//  Copyright © 2017年 zhanghaidi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
